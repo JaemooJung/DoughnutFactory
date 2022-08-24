631bc60d2fdebf51c35b5279df41eedbd651154c
triplecheeseburger <hangyulkim94@gmail.com>
2022-08-24 15:17:17 +0900

    typo

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(void) 
 {
 	printf("There are six problems to solve.\n");
-	printf("Expected result is to display fifteen doughnuts look like this => [ ( 0 ) ] <=\n");
+	printf("Expected result is to display fifteen doughnuts look like this => | ( 0 ) | <=\n");
 	printf("Cheer UP!!\n");
 	go_to_work();
 	return (0);
